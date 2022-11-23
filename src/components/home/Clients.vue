4336fbb1153b8b1fee2133454be2c6684ed1efb2
Oseias Pereira <oseiaspereira88@gmail.com>
2022-11-23 17:32:02 -0300

    -fix

--- src/components/home/Clients.vue
+++ src/components/home/Clients.vue
@@ -1,20 +1,20 @@
 <template>
   <v-container
       id="business-section"
-      class="section pt-1"
+      class="section pt-10"
       style="background: #F1F1F1"
       fluid>
     <h2 class="text-center">CLIENTES</h2>
-    <div class="px-16" style="background: #F1F1F1">
+    <div class="px-0 px-ms-16" style="background: #F1F1F1">
       <v-slide-group show-arrows>
         <v-slide-item v-for="n in 15" :key="n">
-          <v-card :color="'#F1F1F1'" class="mx-4" height="120" width="120" elevation="0">
+          <v-card :color="'#F1F1F1'" class="mx-0 mx-sm-4" height="120" width="120" elevation="0">
             <v-img :src="clients[0].image_src"></v-img>
           </v-card>
         </v-slide-item>
       </v-slide-group>
     </div>
-    <div class="py-12" style="background: #F1F1F1"></div>
+    <div class="pb-12" style="background: #F1F1F1"></div>
   </v-container>
 </template>
 
